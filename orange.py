56aa05686de751ea8cd72cac7031b65daf205174
d10710006 <110797502+d10710006@users.noreply.github.com>
2024-02-03 20:31:42 +0800

    Update orange.py

--- orange.py
+++ orange.py
@@ -1,4 +1,11 @@
-print (" i like to eat orange")
+#Python syntax study 0203224 by Frank
+#use {} as placeholder where you want to insert value
+name="Peter"
+age="11"
+interest="swimming"
+food="fruit"
+print ("{} like to eat {}".format(name,food))
+
 
 
 
